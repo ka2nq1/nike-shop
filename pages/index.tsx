3682d69a07c6f814b7d5067ebd5804384e62d0cd
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-05-31 19:19:45 +0300

    start

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Image from 'next/image';
+// import { Banner, CarouselShoes, DiscountBanner } from '@/sections/home';
+// // import { Header, PreHeader } from '@/ui';
+
+const Home = () => (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+        {/* <PreHeader />
+        <Header />
+        <DiscountBanner />
+        <Banner />
+        <CarouselShoes /> */}
+    </>
+);
+
+export default Home;
